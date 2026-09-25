4d47b1e55bd73fcc64194af1a959e617907dd030
taufiqha <htaufiq225@gmail.com>
2026-09-25 18:03:43 +0800

    update credentials

--- lib/core/config.dart
+++ lib/core/config.dart
@@ -5,7 +5,7 @@ class AppConfig {
   /// Host Laravel. Default 10.0.2.2 = localhost host dari Android emulator.
   static const String host = String.fromEnvironment(
     'API_BASE',
-    defaultValue: 'http://10.204.165.207:8000',
+    defaultValue: 'https://taufiqha.online',
   );
 
   static String get apiRoot => '$host/api/v1';
